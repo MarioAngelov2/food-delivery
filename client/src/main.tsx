23a3527ea870372cd72a05209253493d30dc6e57
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-06-11 21:36:41 +0300

    Add sign up page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout.tsx";
 import SignInPage from "./pages/SignIn.tsx";
+import SignUpPage from "./pages/SignUp.tsx";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <App /> },
       { path: "/sign-in", element: <SignInPage /> },
+      { path: "/sign-up", element: <SignUpPage /> },
     ],
   },
 ]);
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/clerk-react";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+
+export default function SignUpPage() {
+  return (
+    <MaxWidthWrapper>
+      <div className="flex mx-auto w-full mt-36 justify-center min-h-screen">
+        <SignUp />
+      </div>
+    </MaxWidthWrapper>
+  );
+}
