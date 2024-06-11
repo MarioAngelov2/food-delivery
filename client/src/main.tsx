8cde0bc15b388df4faa2aaa39008b1db64dda8df
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-06-11 21:35:03 +0300

    add sign in route

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout.tsx";
+import SignInPage from "./pages/SignIn.tsx";
 
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
-    children: [{ path: "/", element: <App /> }],
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/sign-in", element: <SignInPage /> },
+    ],
   },
 ]);
 
