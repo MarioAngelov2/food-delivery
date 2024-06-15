129a60b84e6d54311abbd0790727504d46c67009
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-06-15 12:37:35 +0300

    Update Navbar.tsx

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,56 @@
-import React from 'react'
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { CiSearch } from "react-icons/ci";
+import { IoBagOutline } from "react-icons/io5";
+import { NavLink, useNavigate } from "react-router-dom";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
+
+const links = [
+  { url: "/", name: "Home" },
+  { url: "/menu", name: "Menu" },
+  { url: "/mobile-app", name: "Mobile App" },
+  { url: "/contact", name: "Contact Us" },
+];
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div>
-        Navbar
-      </div>
-    </nav>
-  )
-}
+  const navigate = useNavigate();
 
-export default Navbar
+  return (
+    <MaxWidthWrapper>
+      <nav className="flex justify-between pt-5 pb-2">
+        <div>
+          <h1 className="text-[#FF823F] font-extrabold text-4xl">Foodie</h1>
+        </div>
+        <div className="flex items-center justify-center gap-6">
+          {links.map((link) => (
+            <NavLink
+              key={link.url}
+              className={({ isActive }) =>
+                isActive ? "text-[#FF823F]" : "text-black"
+              }
+              to={link.url}
+            >
+              {link.name}
+            </NavLink>
+          ))}
+        </div>
+        <div className="flex items-center justify-center gap-8">
+          <CiSearch size={26} />
+          <IoBagOutline size={26} />
+          <SignedOut>
+            <button
+              onClick={() => navigate("/sign-in")}
+              className="border px-5 py-2 rounded-xl border-[#FF823F] text-sm"
+            >
+              Sign In
+            </button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+    </MaxWidthWrapper>
+  );
+};
+
+export default Navbar;
