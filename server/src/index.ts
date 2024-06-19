d81947685a3e4b2d1669d1ccb07b4a66e5c925de
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-06-19 22:22:08 +0300

    connect db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,24 @@
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import express from "express";
 import http from "http";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const MONGO_CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING;
+
+if (!MONGO_CONNECTION_STRING) {
+  console.error("MONGO_CONNECTION_STRING is not set");
+  process.exit(1);
+}
 
 const app = express();
+
+mongoose
+  .connect(MONGO_CONNECTION_STRING)
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("Error connecting to database: ", err));
 
 app.use(bodyParser.json());
 
