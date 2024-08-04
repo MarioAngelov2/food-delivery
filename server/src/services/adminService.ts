f7b02b1d94d1c0036c1b57ad50d2d25ec6bc620f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-08-04 11:27:43 +0300

    Create admin crud service

---
+++ server/src/services/adminService.ts
@@ -0,0 +1,84 @@
+import { ProductModel } from "../models/product.model";
+import { Product } from "../types/index";
+
+export const createProductService = async (data: Product) => {
+  try {
+    if (!data) {
+      throw new Error("Data does not exist");
+    }
+
+    const { name, description, price, image, category } = data;
+
+    const newProduct = new ProductModel({
+      name,
+      description,
+      price,
+      image,
+      category,
+    });
+
+    const savedProduct = await newProduct.save();
+
+    return savedProduct;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database update error");
+  }
+};
+
+export const getAllProductsService = async () => {
+  try {
+    const result = await ProductModel.find();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database update error");
+  }
+};
+
+export const getProductService = async (id: string) => {
+  try {
+    if (!id) {
+      throw new Error("ID does not exist");
+    }
+
+    const product = await ProductModel.findById(id);
+
+    return product;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database update error");
+  }
+};
+
+export const updateProductService = async (data: Product) => {
+  try {
+    if (!data) {
+      throw new Error("Data does not exist");
+    }
+
+    const { name, description, price, image, category, id } = data;
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        description,
+        price,
+        image,
+        category,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedProduct) {
+      throw new Error("Product not found or could not be updated");
+    }
+
+    return updatedProduct;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Database update error");
+  }
+};
