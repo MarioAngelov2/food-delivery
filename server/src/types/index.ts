27f140a3192b566c074bcadb507fa52db2bed04b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-08-04 11:28:02 +0300

    Create types

---
+++ server/src/types/index.ts
@@ -0,0 +1,15 @@
+export type Product = {
+  name: String;
+  description: String;
+  price: Number;
+  image: String;
+  category: String;
+  id?: String;
+}
+
+export type User = {
+  firstName: String;
+  lastName: String;
+  email: String;
+  id: String;
+}
