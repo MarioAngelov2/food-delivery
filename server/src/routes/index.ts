6683a9b64e5bcdb0b931920ed7efe5019b654587
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-08-04 11:27:53 +0300

    Add routes for admin crud

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import bodyParser from "body-parser";
 import { clerkWebhook } from "../controllers/clerkWebhook";
+import {
+  createProduct,
+  updateProduct,
+  getAllProducts,
+  getProduct,
+} from "../controllers/adminController";
 
 const router = Router();
 
@@ -9,5 +15,9 @@ router.post(
   bodyParser.raw({ type: "application/json" }),
   clerkWebhook
 );
+router.post("/admin/create-product", createProduct);
+router.get("/admin/get-all-products", getAllProducts);
+router.get("/admin/get-product/:id", getProduct);
+router.put("/admin/update-product", updateProduct);
 
 export default router;
