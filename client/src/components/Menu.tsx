5d84ba78c44fb29e6be3d2bb958ec7378a887635
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2024-10-08 21:14:07 +0300

    Update Menu.tsx

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -2,20 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { motion } from "framer-motion";
 import { IoCheckmarkOutline } from "react-icons/io5";
-
-const filters = [
-  { name: "Offers1", image: "/offers.webp" },
-  { name: "Offers2", image: "/offers.webp" },
-  { name: "Offers3", image: "/offers.webp" },
-  { name: "Offers4", image: "/offers.webp" },
-  { name: "Offers5", image: "/offers.webp" },
-  { name: "Offers6", image: "/offers.webp" },
-  { name: "Offers7", image: "/offers.webp" },
-  { name: "Offers8", image: "/offers.webp" },
-  { name: "Offers9", image: "/offers.webp" },
-  { name: "Offers10", image: "/offers.webp" },
-  { name: "Offers11", image: "/offers.webp" },
-];
+import { filters } from "../constants/index";
 
 const Menu = () => {
   const [width, setWidth] = useState(0);
@@ -38,7 +25,7 @@ const Menu = () => {
             ref={carousel}
             drag="x"
             dragConstraints={{ right: 0, left: -width }}
-            className="flex gap-4 pb-6"
+            className="flex gap-4 pb-8"
           >
             {filters.map((filter) => (
               <motion.div
@@ -46,7 +33,8 @@ const Menu = () => {
                 className="flex flex-col gap-2.5 justify-center rounded-xl min-w-[150px] cursor-pointer group"
               >
                 <img
-                  className="px-2 bg-red-200 rounded-xl min-w-[100px] pointer-events-none shadow-lg"
+                  className="px-2 rounded-xl min-w-[100px] pointer-events-none shadow-lg"
+                  style={{ backgroundColor: filter.color }}
                   src={filter.image}
                   alt="logo"
                 />
